f1fd632097a7572d0736c86d0f15e911357a11b9
Nada Hussein Muhammed <nada.hussein.m26@gmail.com>
2025-05-17 21:49:24 +0300

    Update signup_success.js and change the pop variable from var to let

--- static/js/signup_success.js
+++ static/js/signup_success.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Check if URL contains ?registered=1
   if (window.location.search.includes("registered=1")) {
-    var popup = document.getElementById("registeration-success");
+    let popup = document.getElementById("registeration-success");
     popup.style.display = "block";
     setTimeout(function () {
       popup.style.display = "none";
