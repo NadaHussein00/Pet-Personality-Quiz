73690d354ef3387a9d38dcce8cfdf713917162e9
Nada Hussein Muhammed <nada.hussein.m26@gmail.com>
2025-05-18 10:33:56 +0300

    Update helpers.py and edit the hash password function logic

--- app/helpers.py
+++ app/helpers.py
@@ -14,41 +14,17 @@ def save_json_file(filepath, data):
     json.dump(data, file, indent=2)
     file.close()
 
-#This file contains the made-from-scratch hash function.
-#First, each character is mapped to its ascii value
-#Second, each ascii value is then multiplied to 16
-#Third, each number that is processed from the second step, is mapped to its hexa digits that represent it
-#and then the final produced hexa number is reversed.
 
-def int_to_hex_char(n):
-    if 0 <= n <= 9:
-        return chr(ord('0') + n)
-    elif 10 <= n <= 15:
-        return chr(ord('a') + (n - 10))
-    else:
-        return '?'
+def hash_password(password):
+    ascii_values = []  # List to hold ASCII codes as strings
 
-def to_hex(num):
-    if num == 0:
-        return '0'
-    hex_digits = []
-    while num > 0:
-        remainder = num % 16
-        hex_digits.append(int_to_hex_char(remainder))
-        num = num // 16
-    hex_digits.reverse()
-    return ''.join(hex_digits)
+    for char in password:
+        ascii_code = ord(char)       # Get ASCII integer value of the character
+        ascii_str = str(ascii_code)  # Convert ASCII code to string
+        ascii_values.append(ascii_str)  # Append to the list
 
-def hash_password(s):
-    result = []
-    for ch in s:
-        ascii_val = ord(ch)          
-        multiplied = ascii_val * 16  
-        hex_str = to_hex(multiplied) 
-        result.append(hex_str)
-    return ''.join(result)	
-
-
+    hashed_password = ''.join(ascii_values)  # Concatenate all ASCII strings into one
+    return hashed_password
 
 def get_html(page_name):
     templates_folder = "templates"
